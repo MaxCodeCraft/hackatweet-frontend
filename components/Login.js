c01b284c1a8f931be751d0bf9f4ebb141e18b6cb
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-05-02 10:32:06 +0200

    pages added

---
+++ components/Login.js
@@ -0,0 +1,13 @@
+function Login() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default Login;
---
+++ pages/home.js
@@ -0,0 +1,7 @@
+import Home from "../components/Home";
+
+function Home() {
+  return <Home />;
+}
+
+export default Home;
